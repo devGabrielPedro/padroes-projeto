79d150feec567bb2661cec026d405d7865ca1cf7
Gabriel Pedro <gabrielpedrodsantos@gmail.com>
2023-10-20 17:52:02 -0300

    projeto de padroes de projeto finalizado

---
+++ src/main/java/one/dio/gof/service/ClienteService.java
@@ -0,0 +1,22 @@
+package one.dio.gof.service;
+
+import one.dio.gof.model.Cliente;
+
+/**
+ * 
+ * @author devGabrielPedro
+ */
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
